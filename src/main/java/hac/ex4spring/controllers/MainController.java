bdf1536e746aa3a014f8789e5200c3c2a34fe789
Matan Tenenboim <matante@edu.hac.ac.il>
2022-05-31 20:30:40 +0300

    commit

--- src/main/java/hac/ex4spring/controllers/MainController.java
+++ src/main/java/hac/ex4spring/controllers/MainController.java
@@ -1,27 +1,17 @@
 package hac.ex4spring.controllers;
 
-import hac.ex4spring.repo.Candy;
+import hac.ex4spring.repo.Sweet;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.validation.Valid;
 
 @Controller
 public class MainController {
+
     @GetMapping("/")
-    public String main(Candy candy) {
-        return "index";
+    public String main(Sweet sweet) {
+        return "homepage";
     }
 
-    @PostMapping("/addcandy")
-    public String addCandy(@Valid Candy candy, Model model, BindingResult result) {
-        if (result.hasErrors()){
-            System.out.println("in error");
-            return "index";
-        }
-        return "index";
-    }
+
 }
